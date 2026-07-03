136fb06bd8cd2a9e8200238f3710a38db441a4e6
sahoosohan <sohanxsahoo@gmail.com>
2026-07-03 20:21:38 +0530

    config Clerk, Postgres using NeonDB and Docker for alternative understanding

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <Button>Hello Internet</Button>
+      {/* <Button>Hello Internet</Button> */}
+      <UserButton />
     </div>
   );
 }
